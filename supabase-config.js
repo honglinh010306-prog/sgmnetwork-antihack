d684ee302150ec078d233d66d8201a4f95d2b115
honglinh010306 <honglinh010306@gmail.com>
2026-09-07 17:20:09 +0700

    fix: update Supabase project credentials and add cache-busting

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
 window.SUPABASE_CONFIG = {
-  url: 'https://nccdkkqmzpuwqqjguxtt.supabase.co',
-  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5jY2Rra3FtenB1d3Fxamd1eHR0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg2MjQ1NjAsImV4cCI6MjEwNDIwMDU2MH0.EzWYyhigsFFa9JaQAHWLezfxWlfzHziX1JlHV06LU2g'
+  url: 'https://vsxwpuajraykwgoorizg.supabase.co',
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZzeHdwdWFqcmF5a3dnb29yaXpnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY2MTAzOTMsImV4cCI6MjA5MjE4NjM5M30.wfEiMwiFSmuRGUBlqPKnm3ZbCoRDRfLIDI4wWMBtC6s'
 };
