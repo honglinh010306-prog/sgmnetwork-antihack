97ae30a7ed5fa720e24b215f9ec2463127f8441a
honglinh010306 <honglinh010306@gmail.com>
2026-09-07 16:39:54 +0700

    fix: update supabase anon key and hide admin main before login

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
 window.SUPABASE_CONFIG = {
   url: 'https://nccdkkqmzpuwqqjguxtt.supabase.co',
-  anonKey: 'sb_publishable_qbWDIQtGpfqL1hcZE37QJA_1oif_m9c'
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5jY2Rra3FtenB1d3Fxamd1eHR0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg2MjQ1NjAsImV4cCI6MjEwNDIwMDU2MH0.EzWYyhigsFFa9JaQAHWLezfxWlfzHziX1JlHV06LU2g'
 };
